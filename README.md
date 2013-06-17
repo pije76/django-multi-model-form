c3fd5d32d9e318b8fd99aa2fa6dab55f98dfaa6c
Ondrej Sika <sika.ondrej@gmail.com>
2013-06-17 12:43:02 +0200

    init empty package

---
+++ README.md
@@ -0,0 +1,14 @@
+django-multi-model-form
+=======================
+
+Joining multiple ModelForm
+
+Authors
+-------
+*  Ondrej Sika, <http://ondrejsika.com>, dev@ondrejsika.com
+
+Source
+------
+* Documentation: <http://ondrejsika.com/docs/django-multi-model-form>
+* Python Package Index: <http://pypi.python.org/pypi/django-multi-model-form>
+* GitHub: <https://github.com/sikaondrej/django-multi-model-form>
---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "django-multi-model-form",
+    version = "1.0.0",
+    url = 'http://ondrejsika.com/docs/django-multi-model-form/',
+    download_url = 'https://github.com/sikaondrej/django-multi-model-form/',
+    license = 'GNU LGPL v.3',
+    description = "",
+    author = 'Ondrej Sika',
+    author_email = 'dev@ondrejsika.com',
+    packages = find_packages(),
+    requires = [],
+    include_package_data = True,
+    zip_safe = False,
+)
