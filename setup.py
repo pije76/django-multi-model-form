f27ba97122f19a4a37088731fa28ba76a0635920
Ondrej Sika <sika.ondrej@gmail.com>
2013-06-17 17:30:34 +0200

    v.1.0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "django-multi-model-form",
-    version = "1.0.0",
+    version = "1.0.1",
     url = 'http://ondrejsika.com/docs/django-multi-model-form/',
     download_url = 'https://github.com/sikaondrej/django-multi-model-form/',
     license = 'GNU LGPL v.3',
